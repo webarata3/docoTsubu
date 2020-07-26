b123c83a09cfa6e25b4aa3f30bb20840a6d8a858
webarata3 <webarata3@gmail.com>
2020-07-26 11:15:22 +0900

    DAOの変更

--- README.md
+++ README.md
@@ -94,4 +94,73 @@
 +        response.sendRedirect(getServletContext().getContextPath() + "/Main");
 ```
 
+## DAOの変更
 
+全体的な見直し。
+- `Connectin`取得をメソッドに切り出し
+- `Connection`、`ResultSet`等をtry-with-resourcesに変更
+- `Class.forName`の除去
+
+```java
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Mutter;
+
+public class MutterDAO {
+    private final String DRIVER_NAME = "org.h2.Driver";
+    private final String JDBC_URL = "jdbc:h2:file:C:/first/2019/Servlet/DB/jugyo;AUTO_SERVER=TRUE";
+    private final String DB_USER = "sa";
+    private final String DB_PASS = "";
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+    }
+
+    public List<Mutter> findAll() {
+        List<Mutter> mutterList = new ArrayList<Mutter>();
+        String sql = "SELECT id, name, text FROM mutter ORDER BY id DESC";
+        try (Connection conn = getConnection();
+                PreparedStatement pStmt = conn.prepareStatement(sql);
+                ResultSet rs = pStmt.executeQuery()) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String userName = rs.getString("name");
+                String text = rs.getString("text");
+                Mutter mutter = new Mutter(id, userName, text);
+                mutterList.add(mutter);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return mutterList;
+    }
+
+    public boolean create(Mutter mutter) {
+        String sql = "INSERT INTO mutter(name, text) VALUES(?, ?)";
+        try (Connection conn = getConnection(); PreparedStatement pStmt = conn.prepareStatement(sql)) {
+            pStmt.setString(1, mutter.getUserName());
+            pStmt.setString(2, mutter.getText());
+
+            int result = pStmt.executeUpdate();
+
+            if (result != 1) {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
+```
--- src/dao/MutterDAO.java
+++ src/dao/MutterDAO.java
@@ -16,18 +16,16 @@ public class MutterDAO {
     private final String DB_USER = "sa";
     private final String DB_PASS = "";
 
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+    }
+
     public List<Mutter> findAll() {
-        Connection conn = null;
         List<Mutter> mutterList = new ArrayList<Mutter>();
-        try {
-            Class.forName(DRIVER_NAME);
-            conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
-
-            String sql = "SELECT id, name, text FROM mutter ORDER BY id DESC";
-            PreparedStatement pStmt = conn.prepareStatement(sql);
-
-            ResultSet rs = pStmt.executeQuery();
-
+        String sql = "SELECT id, name, text FROM mutter ORDER BY id DESC";
+        try (Connection conn = getConnection();
+                PreparedStatement pStmt = conn.prepareStatement(sql);
+                ResultSet rs = pStmt.executeQuery()) {
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String userName = rs.getString("name");
@@ -38,29 +36,13 @@ public class MutterDAO {
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
         }
         return mutterList;
     }
 
     public boolean create(Mutter mutter) {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
-
-            String sql = "INSERT INTO mutter(name, text) VALUES(?, ?)";
-            PreparedStatement pStmt = conn.prepareStatement(sql);
+        String sql = "INSERT INTO mutter(name, text) VALUES(?, ?)";
+        try (Connection conn = getConnection(); PreparedStatement pStmt = conn.prepareStatement(sql)) {
             pStmt.setString(1, mutter.getUserName());
             pStmt.setString(2, mutter.getText());
 
@@ -72,16 +54,8 @@ public class MutterDAO {
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    return false;
-                }
-            }
         }
+
         return true;
     }
 }
